227e27bdb1447435c5495fe514e404dbb422659f
Yosuke Nagaishi <yosuke.nagaishi@gmail.com>
2015-10-03 22:27:43 +0900

    add base title

--- test/controllers/search_pages_controller_test.rb
+++ test/controllers/search_pages_controller_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class SearchPagesControllerTest < ActionController::TestCase
+
+def setup
+  @base_title = "Ginvite"
+end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ginvite"
+    assert_select "title", "Home | #{@base_title}"
   end
 
 end
